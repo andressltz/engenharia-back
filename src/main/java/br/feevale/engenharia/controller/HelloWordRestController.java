342066723612905a27bd00db6cac3639827ad1d6
andresschultz <andreswinck@gmail.com>
2016-10-04 19:48:06 -0300

    estrutura

---
+++ src/main/java/br/feevale/engenharia/controller/HelloWordRestController.java
@@ -0,0 +1,15 @@
+package br.feevale.engenharia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "hello")
+public class HelloWordRestController {
+
+	@RequestMapping(value = "/word", method = RequestMethod.GET)
+	public String helloWord(){
+		return "helloWord";
+	}
+	
+}
---
+++ src/main/java/br/feevale/engenharia/model/Aluno.java
@@ -0,0 +1,11 @@
+package br.feevale.engenharia.model;
+
+//@Entity
+public class Aluno {
+	
+//	@Id
+	private Long idAluno;
+	
+	private String nome;
+
+}
