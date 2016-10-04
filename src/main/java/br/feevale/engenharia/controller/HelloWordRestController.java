d997c2b6ad3a24f78fad38b1e2e9558944d0088b
andresschultz <andreswinck@gmail.com>
2016-10-04 20:04:58 -0300

    adicionado endereco ao restController

--- src/main/java/br/feevale/engenharia/controller/HelloWordRestController.java
+++ src/main/java/br/feevale/engenharia/controller/HelloWordRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "hello")
 public class HelloWordRestController {
 
 	@RequestMapping(value = "/word", method = RequestMethod.GET)
