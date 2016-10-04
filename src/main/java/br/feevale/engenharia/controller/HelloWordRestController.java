134ba8e2d85f96242c0bf7181a0266c7068cecbd
andresschultz <andreswinck@gmail.com>
2016-10-04 19:53:39 -0300

    url de teste

--- src/main/java/br/feevale/engenharia/controller/HelloWordRestController.java
+++ src/main/java/br/feevale/engenharia/controller/HelloWordRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "hello")
+@RestController
 public class HelloWordRestController {
 
 	@RequestMapping(value = "/word", method = RequestMethod.GET)
